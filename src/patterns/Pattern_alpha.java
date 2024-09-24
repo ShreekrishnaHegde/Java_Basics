b1c769e36b3f88c388d0f333bb69784fb69bc52a
Shreekrishna <hegdeshreekrishna469@gmail.com>
2024-09-24 10:09:09 +0530

    Pattern-Alpha

--- src/patterns/Pattern_alpha.java
+++ src/patterns/Pattern_alpha.java
@@ -4,6 +4,10 @@ public class Pattern_alpha {
     public static void main(String[] args) {
         int n=6;
         pattern_1(n);
+        pattern_2(n);
+        pattern_3(n);
+        pattern_4(n);
+        pattern_5(n);
     }
 
     static void pattern_1(int n){
@@ -17,10 +21,79 @@ public class Pattern_alpha {
             for (int col = 1; col <=row; col++) {
                 System.out.print("* ");
             }
-            System.out.println("");
+            System.out.println();
+
         }
+        System.out.println();
 
     }
 
+    static void pattern_2(int n){
+        /*
+        * * * *
+        * * * *
+        * * * *
+         */
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <=n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static void pattern_3(int n){
+        /*
+        * * * *
+        * * *
+        * *
+        *
+         */
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <=n-row+1; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+    static void pattern_4(int n)
+    {
+        /*
+        1
+        1 2
+        1 2 3
+         */
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <=row; col++) {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
+
+    static void pattern_5(int n){
+        /*
+        *
+        * *
+        * * *
+        * *
+        *
+         */
+        for (int row = 1; row <=2*n-1; row++) {
+            int totalCol=row > n ? 2*n-row : row;
+            for (int col = 1; col <=totalCol; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
 
 }
